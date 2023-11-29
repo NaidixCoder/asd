19639c4d4bedfb11c620ae50a525860ed8a4320f
Naidix Coder <matiinfante@gmail.com>
2023-11-29 02:48:36 -0300

    V1.0 pendiente lista sugerencia NavBar

--- main.js
+++ main.js
@@ -10,34 +10,75 @@ const buscadorInicio = getElement("welcome-btn");
 const buscador = getElement("nav-btn");
 const inputNav = getElement("nav-search");
 
+const iconoBusqueda = getElement("bx-menu"); // Icono busqueda
+const header = document.querySelector("header");
+const navBar = document.querySelector(".nav")
+const logo = document.querySelector(".logo")
+
 let ciudad;
 
-// Listeners
-buscadorInicio.addEventListener("click", () =>  buscadorWelcome());
+// Listeners ventana modal.
+buscadorInicio.addEventListener("click", () => buscadorWelcome()); // Evento click, boton buscar de ventana modal.
 
-inputInicio.addEventListener("keydown", function (event) {
+inputInicio.addEventListener("keydown", function (event) { // Evento Keydown, input de ventana modal.
     if (event.key === "Enter") {
         event.preventDefault();
         buscadorWelcome();
     }
 });
 
-inputInicio.addEventListener("input", function () {
+inputInicio.addEventListener("input", function () { // Evento input, boton buscar de ventana modal.
     const textInput = this.value.toLowerCase();
     autoComplete(textInput);
 });
 
-buscador.addEventListener("click", () => searchNav());
+buscador.addEventListener("click", () => searchNav()); // Evento click, boton buscar de navBar.
 
-inputNav.addEventListener("keydown", function (event) {
+// Listeners main page.
+inputNav.addEventListener("keydown", function (event) { // Evento KeyDown, input navBar
     if (event.key === "Enter") {
         event.preventDefault();
         searchNav();
+        resetHeader()
     }
 });
 
+iconoBusqueda.addEventListener("click", () => { // Icono de busqueda. (lupa)
 
-function buscadorWelcome () {
+    iconoBusqueda.classList.add("d-none");
+
+    logo.style.opacity = "0";
+    setTimeout(() => {
+        logo.classList.add("d-none");
+    }, 400)
+
+    inputNav.style.opacity = "1";
+    setTimeout(() => {
+        inputNav.classList.remove("d-none");
+        inputNav.style.display = "inline";
+        inputNav.value = "";
+    }, 400)
+
+    header.style.justifyContent = "center";
+
+})
+
+
+// Funcion para resetear el header luego de una busqueda en mobile.
+function resetHeader() {
+    iconoBusqueda.classList.remove("d-none");
+    if (window.innerWidth < 660) {
+        inputNav.classList.add("d-none");
+    }
+    inputNav.value = "";
+    logo.classList.remove("d-none");
+    logo.style.opacity = "1";
+
+    header.style.justifyContent = "end";
+}
+
+// Funcion de busqueda en ventana modal.
+function buscadorWelcome() {
     ciudad = getElement("welcome-search").value;
 
     if (inputInicio.value !== "") {
@@ -46,28 +87,26 @@ function buscadorWelcome () {
         inputInicio.classList.add("ph-red");
         setTimeout(() => {
             inputInicio.classList.remove("ph-red");
-        }, 400);
+        }, 500);
     }
-    
 }
-function searchNav () {
-    const buscadorNav = getElement("nav-search");
-    ciudad = buscadorNav.value;
-    
 
-    if (buscadorNav.value !== "") {
+// Funcion de busqueda en navBar
+function searchNav() {
+
+    ciudad = inputNav.value;
+
+    if (inputNav.value !== "") {
         conexionApi(ciudad);
-        console.log(buscadorNav.value)
     } else {
-        buscadorNav.classList.add("ph-red")
+        inputNav.classList.add("ph-red")
         setTimeout(() => {
-            buscadorNav.classList.remove("ph-red");
+            inputNav.classList.remove("ph-red");
         }, 500);
     };
 }
 
 // funcion de autompletado.
-
 const autoComplete = (textInput) => {
     const path = "./db/ciudades-argentinas.json";
 
@@ -93,10 +132,10 @@ const autoComplete = (textInput) => {
                 // Muestra las sugerencias en la lista
                 mostrarSugerencias(primerasCiudades);
             }
-
         });
 };
 
+// Funcion para limpiar <li> generados por autocompletado
 const limpiarLista = () => sugerenciasLista.innerHTML = '';
 
 const mostrarSugerencias = (ciudades) => {
@@ -123,6 +162,7 @@ const mostrarSugerencias = (ciudades) => {
     }
 };
 
+// Conexion a API openwheater
 const conexionApi = (ciudad) => {
 
     const KEY = "c06512b6efa1d5cab420470cc4ba5fc5"
@@ -142,19 +182,62 @@ const conexionApi = (ciudad) => {
             description.textContent = traductorClima(data.weather[0].description); // English to spanish
             changeDisplay();
 
-            console.log(data) // Delete after
         })
         .catch(error => {
             console.warn("Error en la conexión:", error.message);
             inputInicio.classList.add("ph-red");
             inputInicio.value = "";
             inputInicio.placeholder = "Ciudad no encontrada";
+            
+            inputNav.classList.add("ph-red");
+            inputNav.value = "";
+            inputNav.placeholder = "Ciudad no encontrada";
+            
             setTimeout(() => {
                 inputInicio.placeholder = "Ingrese localidad"
                 inputInicio.classList.remove("ph-red");
+                
+                inputNav.classList.remove("ph-red");
+                inputNav.placeholder = "Ingrese localidad";
+
             }, 1000);
             return
         });
+};
+
+// Funcion para acutalizar los datos del clima.
+const actualizarDatosClima = (data) => {
+    const temperatura = getElement("temperatura");
+    const humedad = getElement("humedad");
+    const wind = getElement("wind");
+    const titulo = getElement("title");
+    const imgMain = getElement("main-img");
+
+    titulo.textContent = `${data.name}, ${data.sys.country}`;
+    imgMain.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    temperatura.textContent = parseFloat(data.main.temp / 10).toFixed(2) + "°";
+    humedad.textContent = `${data.main.humidity}%`;
+    wind.textContent = `${(data.wind.speed * 1.60934).toFixed(1)} Kmh`;
+};
+
+// Traduce las respuestas recibidas de la API. English to Spanish.
+const traductorClima = (englishDescription) => {
+    switch (englishDescription) {
+        case "scattered clouds":
+            return "Nubes dispersas";
+        case "overcast clouds":
+            return "Mayormente nublado";
+        case "broken clouds":
+            return "Parcialmente nublado";
+        case "few clouds":
+            return "Pocas nubes";
+        case "clear sky":
+            return "Despejado";
+        case "light rain":
+            return "LLuvia debil";
+        default:
+            return englishDescription;
+    }
 };
 
 
@@ -163,13 +246,18 @@ const changeDisplay = () => {
     const modalWelcome = document.querySelector(".modalInicio"); // Ventana de inicio de pagina.
     const welcomeContent = document.querySelector(".welcome"); // Selector de contenido dentro de modal.
     const mainContent = document.querySelector(".main-content"); // Selector del contenido dentro de <main> .
-    const header = document.querySelector(".header"); // Selector del <header>
+
+    const main = document.querySelector(".main")
 
     modalWelcome.style.opacity = "0";
-    header.style.display = "flex";
 
-    modalWelcome.style.height = "calc(100vh - 90px)";
+    header.classList.remove("d-none");
+    header.classList.add("header");
     header.style.opacity = "1";
+
+    main.classList.remove("d-none")
+    mainContent.classList.remove("d-none")
+
     mainContent.style.opacity = "1";
     welcomeContent.style.opacity = "0";
 
@@ -178,33 +266,3 @@ const changeDisplay = () => {
         modalWelcome.style.display = "none";
     }, 2000);
 }
-
-
-// Funcion traductor.
-
-const traductorClima = (englishDescription) => {
-    switch (englishDescription) {
-        case "scattered clouds":
-            return "Nubes dispersas";
-        case "broken clouds":
-            return "Parcialmente nublado";
-        case "clear sky":
-            return "Despejado";
-        default:
-            return englishDescription;
-    }
-};
-
-const actualizarDatosClima = (data) => {
-    const temperatura = getElement("temperatura");
-    const humedad = getElement("humedad");
-    const wind = getElement("wind");
-    const titulo = getElement("title");
-    const imgMain = getElement("main-img");
-
-    titulo.textContent = data.name;
-    imgMain.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    temperatura.textContent = parseFloat(data.main.temp / 10).toFixed(2) + "°";
-    humedad.textContent = `${data.main.humidity}%`;
-    wind.textContent = `${(data.wind.speed * 1.60934).toFixed(1)} Kmh`;
-};
